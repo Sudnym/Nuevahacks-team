85c2574306897a07bb1892df42273e714dc7f509
Sudnym <mathescole@gmail.com>
2020-04-10 23:54:06 -0400

    Made a few designs, made a header(not final just playing around)

--- svelte-frontend/src/main.svelte
+++ svelte-frontend/src/main.svelte
@@ -1,1 +1,9 @@
 <!-- This is the main page where users view algorithmically chosen posts -->
+
+<script>
+    import Box from './styling.svelte'
+</script>
+
+<Box>
+    <h1><strong>We're all done for today?</strong></h1>
+</Box>
---
+++ svelte-frontend/src/styling.svelte
@@ -0,0 +1,39 @@
+<style>
+    .box {
+        width: 98%;
+        border: 1px solid #aaa;
+        border-radius: 25px;
+        box-shadow: 2px 2px 8px rgba(0,0,0,0.1);
+        padding: 1em;
+        margin: 0 0 1em 0;
+        background: #333333;
+        align-content: center;
+        align-items: center;
+        justify-content: center;
+        alignment-baseline: center;
+        place-content: center;
+    }
+    .box-inside {
+        border-radius: 25px;
+        width: 100%;
+        border: none;
+        background: #666666;
+        align-self: center;
+        align-content: center;
+        display: inline-block;
+        justify-content: center;
+        text-align: center;
+        margin: auto;
+        color: floralwhite;
+    }
+    body {
+        background-color: coral;
+    }
+</style>
+<body>
+<div class="box">
+    <div class="box-inside">
+        <slot></slot>
+    </div>
+</div>
+</body>
