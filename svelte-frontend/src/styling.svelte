891ae9900583630937ec7312c85713651e58ead0
Dev-Excelsius <49457458+Zaphikel@users.noreply.github.com>
2020-04-11 22:29:38 -0400

    Update styling.svelte
    Updated the header; started navigation bar, etc.

--- svelte-frontend/src/styling.svelte
+++ svelte-frontend/src/styling.svelte
@@ -1,23 +1,84 @@
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+</head>
+
 <style>
+    #top-nav-bar {
+        background-color: rgb(252, 252, 252);
+        height: 8rem;
+        width: 100%;
+        margin-left: -1.2rem;
+        margin-top: -1.2rem;
+        border-style: solid;
+        border-right-width: 1rem;
+        border-color: #1D2951;
+        position: fixed;
+        z-index: 10;
+            
+    }
+    #top-nav {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        float: left;
+        color: #588BAE;
+        padding: 9px;
+        padding-left: 75px;
+        font-family: "Trebuchet MS", Helvetica, sans-serif;
+        text-decoration: none;
+        text-shadow: 0px 0px 5px black;
+        font-size: 1.25vw;
+        
+    }
     .box {
-        width: 98%;
-        border: 1px solid #005050;
-        border-radius: 25px;
-        box-shadow: 2px 2px 8px rgba(0,0,0,0.1);
-        padding: 1em;
-        margin: 0 0 1em 0;
-        background: dimgray;
+        width: 100%;
+        border: 1px solid  rgb(190, 190, 190);
+        box-shadow: 0px 0px 8px rgba(0,0,0,1);
+        background: rgb(190, 190, 190);
         align-content: center;
         align-items: center;
         justify-content: center;
-        alignment-baseline: center;
+        place-content: center;
+    }
+    #heading { 
+        background-color: #95C8D8;
+        width: 100%;
+        margin-left: -10px;
+        margin-top: 35px;                    
+        border-style: solid;                    
+        border-right-width: 20px;
+        border-color: #95C8D8;
+        position: fixed;
+        height: 100px;
+        z-index: 11;
+        padding-left: 600px;
+        text-decoration: none;
+        font-family: "Trebuchet MS", Helvetica, sans-serif;
+        box-shadow: 0 0 0px black;
+        
+    }
+    #logo {
+        margin-top: 2rem;
+        margin-left: 1rem;
+        width: 4rem;
+        height: 4rem;
+        z-index: 15;
+    
+    }
+    .box-white {
+        width: 100%;
+        border: 1px solid #005050;
+        box-shadow: 0px 0px 8px rgba(0,0,0,1);
+        background: #EEEEEE;
+        align-content: center;
+        align-items: center;
+        justify-content: center;
         place-content: center;
     }
     .box-inside {
-        border-radius: 25px;
         width: 100%;
         border: none;
-        background: #1d3040;
+        background: #EEEEEE;
         align-self: center;
         align-content: center;
         display: inline-block;
@@ -27,14 +88,30 @@
         color: dimgrey;
     }
     :global(body){
-        background: #1d3040;
+        background: #141414;
     }
     .headertext{
         font-family: Calibri;
     }
+    .heading-header-text{
+        position: absolute;
+        margin-top: 2.5rem;
+        margin-left: 6rem;
+        color: #495059;
+        font-family: 'Montserrat', sans-serif
+    }
 </style>
 <body>
-    <div class="box">
+    <div id="top-nav-bar">
+        <div id = "heading-logo" >
+            <h1 class = "heading-header-text"> Athena </h1>
+            <img id = "logo" src = "https://www.pngitem.com/pimgs/m/260-2606767_greek-pillar-capital-icon-pillar-greece-png-icon.png" alt="Logo">
+        </div>
+        <nav id="top-nav">
+        </nav>
+    </div>
+
+    <div class="box-white">
         <div class="box-inside">
             <h1 class="headertext">
                 <strong>
@@ -42,5 +119,7 @@
                 </strong>
             </h1>
         </div>
+    </div>
+    <div class = "box">
     </div>
 </body>
