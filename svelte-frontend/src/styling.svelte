e6af6c74987fe21c7350d8985e8e4fc542f52a95
Dev-Excelsius <49457458+Zaphikel@users.noreply.github.com>
2020-04-13 18:33:43 -0400

    Update styling.svelte
    Updated UI with navigation bar.

--- svelte-frontend/src/styling.svelte
+++ svelte-frontend/src/styling.svelte
@@ -9,9 +9,6 @@
         width: 100%;
         margin-left: -1.2rem;
         margin-top: -1.2rem;
-        border-style: solid;
-        border-right-width: 1rem;
-        border-color: #1D2951;
         position: fixed;
         z-index: 10;
             
@@ -21,7 +18,7 @@
         margin: 0;
         padding: 0;
         float: left;
-        color: #588BAE;
+        color: #ccb63a;
         padding: 9px;
         padding-left: 75px;
         font-family: "Trebuchet MS", Helvetica, sans-serif;
@@ -30,24 +27,50 @@
         font-size: 1.25vw;
         
     }
-    .box {
+    #top-links {
+        position: absolute;
+        padding: 3.05rem;
+        text-shadow: 0px 0px 19px #fff1a4;
+        margin-left: 50%;
+        z-index: 20;
+        font-family: 'Montserrat', sans-serif;
+        font-size: 2rem;
+                        
+    }
+
+    #top-nav-link   {
+        position: relative;
+        color: #ccb63a;
+        text-decoration: none !important;
+        text-shadow: 0px 3px 0px #fff1a4;
+        margin-left: 4rem;
+    
+    }
+        
+    #top-nav-link:hover {color: #fff1a4;
+        text-shadow: 0px 3px 0px #ccb63a;
+    
+    }
+    .box1 {
         width: 100%;
-        border: 1px solid  rgb(190, 190, 190);
+        margin-left: -1.25rem;
+        margin-top: 5rem;
+        border: 1.5rem solid #fff1a4;
         box-shadow: 0px 0px 8px rgba(0,0,0,1);
-        background: rgb(190, 190, 190);
+        background: #fff1a4;
         align-content: center;
         align-items: center;
         justify-content: center;
         place-content: center;
     }
     #heading { 
-        background-color: #95C8D8;
+        background-color: #ccb63a;
         width: 100%;
         margin-left: -10px;
         margin-top: 35px;                    
         border-style: solid;                    
         border-right-width: 20px;
-        border-color: #95C8D8;
+        border-color: #ccb63a;
         position: fixed;
         height: 100px;
         z-index: 11;
@@ -67,9 +90,7 @@
     }
     .box-white {
         width: 100%;
-        border: 1px solid #005050;
         box-shadow: 0px 0px 8px rgba(0,0,0,1);
-        color: #333333;
         background: #EEEEEE;
         align-content: center;
         align-items: center;
@@ -78,6 +99,8 @@
     }
     .box-inside {
         width: 100%;
+        margin-top: -1rem;
+        margin-left: 2rem;
         border: none;
         background: #EEEEEE;
         align-self: center;
@@ -86,7 +109,7 @@
         justify-content: center;
         text-align: center;
         margin: auto;
-        color: dimgrey;
+        color: #fff1a4;
     }
     :global(body){
         background: #141414;
@@ -105,10 +128,20 @@
 <body>
     <div id="top-nav-bar">
         <div id = "heading-logo" >
+            <nav id = "top-links">
+                <a id = "top-nav-link" href = "index.html" style = "color: black;"> <b> Home</b> </a>
+                <a id = "top-nav-link" href = "about-us.html"> <b> Post </b> </a>
+                <a id = "top-nav-link" href = "get-involved.html"> <b> Find Your Post </b> </a>
+                <a id = "top-nav-link" href = "contact.html"> <b> Contact </b> </a>
+            </nav>
             <h1 class = "heading-header-text"> Athena </h1>
             <img id = "logo" src = "https://www.pngitem.com/pimgs/m/260-2606767_greek-pillar-capital-icon-pillar-greece-png-icon.png" alt="Logo">
         </div>
         <nav id="top-nav">
         </nav>
     </div>
+
+    <div class = "box1">
+    </div>
+    <img src = {test.png} alt = "Test">
 </body>
