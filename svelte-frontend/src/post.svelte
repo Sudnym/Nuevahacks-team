5b84c8bb4901de19025bfa8018ca8ad64fbeb0fa
Sudnym <mathescole@gmail.com>
2020-04-16 22:22:55 -0400

    partial post page

--- svelte-frontend/src/post.svelte
+++ svelte-frontend/src/post.svelte
@@ -1,1 +1,22 @@
 <!-- This is the page where users upload their own posts and those posts are then sent to the server for handling -->
+<style>
+    .postbox{
+        width: 50%;
+        height: 300px;
+    }
+</style>
+<div>
+    <label id="title_input">
+        <input placeholder="Title">
+    </label>
+</div>
+<br>
+<div>
+    <label id="post_input">
+        <input class="postbox" placeholder="Post">
+    </label>
+</div>
+<br>
+<div>
+    <button><b>Post</b></button>
+</div>
