8484e247b83faa2797422b98dad6262622d0c71f
Sudnym <mathescole@gmail.com>
2020-04-16 22:39:20 -0400

    Home screen updated

---
+++ svelte-frontend/src/home.svelte
@@ -0,0 +1,15 @@
+<script>
+    let posts = [
+        {title: "this post", text: "this text"}
+    ]
+    function getposts() {
+
+    }
+</script>
+
+<body>
+{#each posts as {title, text}}
+    <h1>{title}</h1>
+    <p>{text}</p>
+{/each}
+</body>
