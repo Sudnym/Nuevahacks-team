cd1848f8d87b8dff9381e7bbeb8b3e6ed4479b1a
Dev-Excelsius <49457458+Zaphikel@users.noreply.github.com>
2020-04-17 21:34:42 -0400

    Update styling.svelte
    The error one

--- svelte-frontend/src/styling.svelte
+++ svelte-frontend/src/styling.svelte
@@ -1,12 +1,12 @@
-<svelte:head>
+<head>
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-</svelte:head>
+</head>
 <script>
     import Post from "./post.svelte"
     import Search from "./search.svelte"
     import Contact from "./contact.svelte"
     import Home from "./home.svelte"
-    let src = "./test.png"
+    let src = "./test.png";
     function homeHandler(){
         document.getElementById("post").style.display = "none";
         document.getElementById("search").style.display = "none";
@@ -34,7 +34,7 @@
 </script>
 <style>
     #top-nav-bar {
-        background-color: #350035;
+        background-color: rgb(252, 252, 252);
         height: 8rem;
         width: 100%;
         margin-left: -1.2rem;
@@ -51,17 +51,8 @@
         padding: 9px 9px 9px 75px;
         font-family: "Trebuchet MS", Helvetica, sans-serif;
         text-decoration: none;
+        text-shadow: 0 0 5px black;
         font-size: 1.25vw;
-
-    }
-    #top-links {
-        position: absolute;
-        padding: 3.05rem;
-        text-shadow: 0 0 19px #fff1a4;
-        margin-left: 50%;
-        z-index: 20;
-        font-family: 'Montserrat', sans-serif;
-        font-size: 2rem;
 
     }
 
@@ -91,23 +82,6 @@
         justify-content: center;
         place-content: center;
     }
-    #heading {
-        background-color: #ccb63a;
-        width: 100%;
-        margin-left: -10px;
-        margin-top: 35px;
-        border-style: solid;
-        border-right-width: 20px;
-        border-color: #ccb63a;
-        position: fixed;
-        height: 100px;
-        z-index: 11;
-        padding-left: 600px;
-        text-decoration: none;
-        font-family: "Trebuchet MS", Helvetica, sans-serif;
-        box-shadow: 0 0 0 black;
-
-    }
     #logo {
         margin-top: 2rem;
         margin-left: 1rem;
@@ -116,33 +90,9 @@
         z-index: 15;
 
     }
-    .box-white {
-        width: 100%;
-        box-shadow: 0 0 8px rgba(0,0,0,1);
-        background: #EEEEEE;
-        align-content: center;
-        align-items: center;
-        justify-content: center;
-        place-content: center;
-    }
-    .box-inside {
-        width: 100%;
-        margin-top: -1rem;
-        margin-left: 2rem;
-        border: none;
-        background: #EEEEEE;
-        align-self: center;
-        align-content: center;
-        display: inline-block;
-        justify-content: center;
-        text-align: center;
-        color: #fff1a4;
-    }
     :global(body){
         background: #141414;
-    }
-    .headertext{
-        font-family: Calibri, serif;
+        font-family: 'Montserrat', sans-serif;
     }
     .heading-header-text{
         position: absolute;
@@ -151,14 +101,15 @@
         color: #495059;
         font-family: 'Montserrat', sans-serif
     }
+    
 </style>
 <body>
     <div id="top-nav-bar">
         <div id = "heading-logo" >
-            <h1 class = "heading-header-text"> Grapevine </h1>
-            <img id = "logo" src = https://cdn.discordapp.com/attachments/698564721334419507/699769787294810162/LogoMakr_5Lb4Lv.png alt="Logo">
+            <h1 class = "heading-header-text"> The Grape Vine </h1>
+            <img id = "logo" src = "https://www.pngitem.com/pimgs/m/260-2606767_greek-pillar-capital-icon-pillar-greece-png-icon.png" alt="Logo">
             <div style="margin: 50px; width: 50%; float: right ">
-                <a id = "top-nav-link" style = "color: black;" on:click={homeHandler}> <b> Home</b> </a>
+                <a id = "top-nav-link" style = "color: rgb(112, 49, 117);" on:click={homeHandler}> <b> Home</b> </a>
                 <a id = "top-nav-link" on:click={postHandler}> <b> Post </b> </a>
                 <a id = "top-nav-link" on:click={searchHandler}> <b> Find Your Post </b> </a>
                 <a id = "top-nav-link" on:click={contactHandler}> <b> Contact </b> </a>
@@ -178,7 +129,7 @@
     <div id="contact" style="display: none">
         <Contact/>
     </div>
-    <div id="home" style="display: block">
+    <div id="home" style="display: none">
         <Home/>
     </div>
 </body>
